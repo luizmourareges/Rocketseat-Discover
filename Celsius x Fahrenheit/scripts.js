9dbc2b10e9b1f7da15427e229d3b8e7ef1158bb1
luizmourareges <luizmourareges@gmail.com>
2022-08-24 22:47:15 -0300

    novo exercicio

--- Celsius x Fahrenheit/scripts.js
+++ Celsius x Fahrenheit/scripts.js
@@ -1,4 +1,32 @@
 function transformDegree(degree){
     const celsiusExists = degree.toUpperCase().includes('C')
     const fahrenheitExists = degree.toUpperCase().includes('F')
+
+    // fluxo de erro
+    if(!celsiusExists && !fahrenheitExists){
+        throw new Error('Grau não identificado')
+    }
+
+    //fluxo ideal, F -> C
+    let updateDegree = Number(degree.toUpperCase().replace("F", ""));
+    let formula = fahrenheit => (fahrenheit - 32) * 5/9
+    let degreeSign = 'C'
+
+    //fluxo alternativo
+    if(celsiusExists){
+        updateDegree = Number(degree.toUpperCase().replace("C", ""));
+        formula = celsius => celsius * 9/5 + 32
+        degreeSign = 'F'        
+    }
+
+    return formula(updateDegree) + degreeSign
+
+}
+
+try {
+    console.log(transformDegree('10C'))
+    console.log(transformDegree('50F'))
+    transformDegree('50Z')
+} catch (error) {
+    console.log(error.message)
 }
