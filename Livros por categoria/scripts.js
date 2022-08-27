2e1d75f2100e3b45045561f48a37e3890443dda0
luizmourareges <luizmourareges@gmail.com>
2022-08-27 16:43:17 -0300

    novo exercicio

---
+++ Livros por categoria/scripts.js
@@ -0,0 +1,19 @@
+const bookByCategory = [
+    {
+        category: "Riqueza"
+        books: [
+            {
+                title: "Os segredos da mente milionári"
+                author: "T. Harv Eker"
+            },            
+            {
+                title: "O homem mais rico da Babilônia"
+                author: "Gerorge C. Clason"
+            },
+            {
+                title: "Pai rico, pai pobre"
+                author: "Robert T. Kiyosaki e Sharon L. Lechter"
+            },
+        ] 
+    }
+]
